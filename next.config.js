188b87547ed5645f5b175ac0ff0265cbb8888c7a
R.RIEDEL <riedel.ruben@gmail.com>
2026-01-03 12:25:55 +0100

    Make basePath/assetPrefix conditional for prod only

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production' || process.env.GITHUB_ACTIONS === 'true'
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-  basePath: '/LMENC',
-  assetPrefix: '/LMENC',
+  // only apply basePath/assetPrefix in production (GitHub Pages)
+  ...(isProd ? { basePath: '/LMENC', assetPrefix: '/LMENC' } : {}),
   images: {
     unoptimized: true
   }
